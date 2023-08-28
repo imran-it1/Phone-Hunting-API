589e0b24ce7a8263babfe00dd5d8f200a78a99d9
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-28 18:17:52 +0600

    Doing Modal Related Work

--- JS/phone.js
+++ JS/phone.js
@@ -1,21 +1,21 @@
 // Data load
 
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText = "13", isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
-  showPhone(phones);
+  showPhone(phones, isShowAll);
 };
 
-const showPhone = (phones) => {
+const showPhone = (phones, isShowAll) => {
   // 1. Find the container phone card div
   const phoneContainer = document.getElementById("phone-container");
   // Clear old search data of phone after click the search button
   phoneContainer.textContent = "";
-  console.log(phones);
+  // console.log(phones);
 
   // Button remove korte abar lada function else dite hobe jeta jhamela besi
   // if (phones.length > 12) {
@@ -25,16 +25,30 @@ const showPhone = (phones) => {
   // }
 
   const showAllButton = document.getElementById("show-all-button");
-  if (phones.length > 12) {
+  if (phones.length > 12 && !isShowAll) {
     showAllButton.classList.remove("hidden");
   } else {
     showAllButton.classList.add("hidden");
   }
 
+  // console.log(isShowAll);
   // Show few phones if there is find big phone list
-  phones = phones.slice(0, 12);
+  /**
+   * 1. Normal Search a isShowAll er value hoy undefined. That mean falsy value.
+   * !falsy = true , so er moddhe dhuke slice ta korbe.
+   *
+   * 2. Show all button a click korle tar value set hobe true,
+   * !true = false, so slice hobe na. Sob gulo result show korbe
+   *
+   */
+
+  // True hole er moddhe dhukbe na hole dhukbe na
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
+    // console.log(phone);
     // 2. Creta the child element
     const phoneCard = document.createElement("div");
     // Set class of child element if needed
@@ -49,7 +63,7 @@ const showPhone = (phones) => {
     <p>There are many variations of passages of available, but the majority have suffered</p>
     <p><span class="font-semibold">Price:$999</span></p>
       <div class="card-actions">
-        <button class="btn btn-primary">Show Details</button>
+        <button onclick="handleShowDetails('${phone.slug}')" id ="show-details-button" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `;
@@ -57,6 +71,7 @@ const showPhone = (phones) => {
     phoneContainer.appendChild(phoneCard);
     // Clear phone container if user clear the search input
   });
+  // Spinner End
   handleSpinner(false);
 };
 
@@ -69,7 +84,8 @@ const showPhone = (phones) => {
 // };
 
 // Handle Search Button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
+  // Spinner Start
   handleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
@@ -78,7 +94,7 @@ const handleSearch = () => {
   //   searchField.value = "";
   //   return;
   // }
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 };
 
 // Handle Spinner
@@ -91,4 +107,32 @@ const handleSpinner = (isSpine) => {
   }
 };
 
-// loadPhone();
+// Handle Show All Button
+const handleShowAll = () => {
+  handleSearch(true);
+};
+
+// Handle Show details Button
+const showBetailsButton = document.getElementById("show-details-button");
+
+const handleShowDetails = async (phoneID) => {
+  // console.log(phoneID);
+  // load signle phone data
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${phoneID}`
+  );
+  const data = await res.json();
+  const phone = data.data;
+  // console.log(phone);
+  showDetails(phone);
+};
+
+const showDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById("phone-name");
+  phoneName.innerText = phone.name;
+  // Show Modal
+  show_details_modal.showModal();
+};
+
+loadPhone();
